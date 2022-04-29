aff40a3baf8aa4577f763164b3b288e108237d86
maskods <naufal.120140044@student.itera.ac.id>
2022-04-29 22:08:04 +0700

    MVC and templating with hbs

---
+++ learn-express/routes/students.router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const studentsController = require('../controllers/students.controller');
+const studentsRouter = express.Router();
+
+// Method GET /students
+studentsRouter.get('/', studentsController.getStudents);
+
+// Method GET /students/:id (get student by id)
+studentsRouter.get('/:id', studentsController.getStudent);
+
+// Method POST /students (create new student)
+studentsRouter.post('/', studentsController.postStudents);
+
+module.exports = studentsRouter;
--- learn-express/server.js
+++ learn-express/server.js
@@ -1,26 +1,14 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const studentRouter = require('./routes/students.router');
 const port = 3000;
 
-const students = [
-    {
-        "id": 1,
-        "name": "Naufal Taufiq Ridwan",
-        "age": 20,
-    },
-    {
-        "id": 2,
-        "name": "Naufal Angelos",
-        "age": 21,
-    },
-    {
-        "id": 3,
-        "name": "Yasmine Nadia Putri",
-        "age": 21,
-    }
-];
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // middleware
@@ -32,45 +20,16 @@ app.use((req, res, next) => {
 });
 
 // Method GET 
-app.get('/', (req, res) => {
-    res.send('<h1>Learn Express JS</h1>');
-});
-
-
-// Method GET /students
-app.get('/students', (req, res) => {
-    res.json(students);
-});
-
-// Method GET /students/:id (get student by id)
-app.get('/students/:id', (req, res) => {
-    var studentId = Number(req.params.id) - 1;
-    if(studentId < students.length){
-        res.status(200).json(students[studentId]);
-    } else{
-        res.status(404).json({
-            "error": "Student not found"
+app.use('/', (req, res) => {
+    res.render('index', {
+        title: 'Learn Express JS',
+        content: 'Welcome to my website'
         });
-    }
 });
 
-// Method POST /students (create new student)
-app.post('/students', (req, res) => {
-    if(!req.body.name || !req.body.age){
-        return res.status(400).json({
-            error: "Name and age are required"
-        });
-    }
 
-    console.log(req.body);
-    var newStudent = {
-        "id": students.length + 1,
-        "name": req.body.name,
-        "age": req.body.age
-    }
-    students.push(newStudent);
-    res.sendStatus(200);
-});
+app.use('/students', studentRouter);
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
