277c77088eb4df321ca58e1cc3f39b99b05e7c09
maskods <naufal.120140044@student.itera.ac.id>
2022-04-29 22:06:22 +0700

    MVC and templating with handlebars

---
+++ learn-express/controllers/students.controller.js
@@ -0,0 +1,40 @@
+const studentsModel = require('../models/students.model');
+
+function getStudents(req, res){
+    res.json(studentsModel);
+} 
+
+function getStudent(req, res){
+    var studentId = Number(req.params.id) - 1;
+    if(studentId < studentsModel.length){
+        res.status(200).json(studentsModel[studentId]);
+    } else{
+        res.status(404).json({
+            "error": "Student not found"
+        });
+    }
+}
+
+function postStudents(req, res){
+    if(!req.body.name || !req.body.age){
+        return res.status(400).json({
+            error: "Name and age are required"
+        });
+    }
+
+    console.log(req.body);
+    var newStudent = {
+        "id": studentsModel.length + 1,
+        "name": req.body.name,
+        "age": req.body.age
+    }
+    studentsModel.push(newStudent);
+    res.sendStatus(200);
+}
+
+
+module.exports = {
+    getStudents,
+    getStudent,
+    postStudents
+};
---
+++ learn-express/models/students.model.js
@@ -0,0 +1,19 @@
+const students = [
+    {
+        "id": 1,
+        "name": "Naufal Taufiq Ridwan",
+        "age": 20,
+    },
+    {
+        "id": 2,
+        "name": "Naufal Angelos",
+        "age": 21,
+    },
+    {
+        "id": 3,
+        "name": "Yasmine Nadia Putri",
+        "age": 21,
+    }
+];
+
+module.exports = students;
